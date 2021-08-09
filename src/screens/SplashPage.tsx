4e7568e221842ec5129e5781b7a79bb739c7012b
Sam Henley <samueljhenley@gmail.com>
2021-08-09 19:51:33 -0400

    add quote and styling to splash page

--- src/screens/SplashPage.tsx
+++ src/screens/SplashPage.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { FontAwesome } from '@expo/vector-icons';
 
 import ContainerStyle from '../styles/ContainerStyle';
+import SplashPageStyle from '../styles/SplashPageStyle';
 
 const SplashPage: React.FC = () => {
   const navigation = useNavigation();
 
+  const quote = 'The press is not only free; it is powerful. That power is ours. It is the ' +
+    'proudest that man can enjoy. It was not granted by monarchs, it was not gained for us' +
+    'by aristocracies; but it sprang from the people, and, with an immortal instinct,' +
+    'it has always worked for the people.';
+
   return (
     <View style={ContainerStyle.container}>
-      <Text>Splash Page</Text>
+      <FontAwesome name="newspaper-o" size={100} />
+      <Text style={SplashPageStyle.quote}>{quote}</Text>
+      <Text style={SplashPageStyle.quotedPerson}>- Benjamin Disraeli</Text>
       <Button
-        title="Navigate"
+        title="Show News"
         onPress={(): void => navigation.navigate('NewsFeed')}
       />
     </View>
---
+++ src/styles/SplashPageStyle.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+const SplashPageStyle = StyleSheet.create({
+  quote: {
+    fontStyle: 'italic',
+    width: 300,
+  },
+  quotedPerson: {
+    marginBottom: 10,
+    textAlign: 'right',
+    width: 300,
+  },
+});
+
+export default SplashPageStyle;
